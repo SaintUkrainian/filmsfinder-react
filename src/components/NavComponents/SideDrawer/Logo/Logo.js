650fee5891495e488413fd271b4cb99748410685
Matsenko Denys <idanchik47@gmail.com>
2020-12-06 17:38:48 +0200

    background styling

--- src/components/NavComponents/SideDrawer/Logo/Logo.js
+++ src/components/NavComponents/SideDrawer/Logo/Logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 import logoFF from "../../../../assets/images/logoFF.png";
 
-const logo = () => <img src={logoFF} alt="logo"></img>;
+const logo = () => <img src={logoFF} alt="logo" style={{margin: 10}}></img>;
 
 export default logo;
--- src/components/NavComponents/SideDrawer/SideDrawer.js
+++ src/components/NavComponents/SideDrawer/SideDrawer.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import CSSTransition from "react-transition-group/CSSTransition";
 import "./SideDrawer.css";
+import Logo from "./Logo/Logo";
 
 const SideDrawer = (props) => {
     return (
         <CSSTransition in={props.show} classNames="slider" unmountOnExit mountOnEnter timeout={500}>
             <div className="SideDrawer">
+                <Logo />
+                <h1>This is SideDrawer</h1>
+                <hr></hr>
             </div>
         </CSSTransition>
     );
